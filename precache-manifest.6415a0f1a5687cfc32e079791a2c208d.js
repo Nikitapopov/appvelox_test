796736ef5f3631459777044f2207441e736e3d8a
Nikitapopov <nikitos.popov@inbox.ru>
2020-10-09 15:46:07 +0300

    Updates

---
+++ precache-manifest.6415a0f1a5687cfc32e079791a2c208d.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef04659cb281b249028c6ad83d8e6d77",
+    "url": "/appvelox_test/index.html"
+  },
+  {
+    "revision": "abb84e1ad1c2a6b54e8d",
+    "url": "/appvelox_test/static/css/2.1ce32a85.chunk.css"
+  },
+  {
+    "revision": "ba0c96ddbe57059ad091",
+    "url": "/appvelox_test/static/css/main.f6b1e39c.chunk.css"
+  },
+  {
+    "revision": "abb84e1ad1c2a6b54e8d",
+    "url": "/appvelox_test/static/js/2.eb641284.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/appvelox_test/static/js/2.eb641284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0c96ddbe57059ad091",
+    "url": "/appvelox_test/static/js/main.8dcf291a.chunk.js"
+  },
+  {
+    "revision": "fdc2d621ea3aff621cfa",
+    "url": "/appvelox_test/static/js/runtime-main.b81f02c8.js"
+  },
+  {
+    "revision": "74d63de94892fd3ba8f9e39cc77bbfef",
+    "url": "/appvelox_test/static/media/1.74d63de9.svg"
+  },
+  {
+    "revision": "6e80e35d91824af1569da11c8bbb0d87",
+    "url": "/appvelox_test/static/media/2.6e80e35d.svg"
+  },
+  {
+    "revision": "0a0de37471d2ef4e4723d11910d57919",
+    "url": "/appvelox_test/static/media/Rubik-Light.0a0de374.svg"
+  },
+  {
+    "revision": "a0c8acc4213d47653b2f1c006f1e2d19",
+    "url": "/appvelox_test/static/media/Rubik-Light.a0c8acc4.woff2"
+  },
+  {
+    "revision": "b3de0322bd71105c0ca669867e466409",
+    "url": "/appvelox_test/static/media/Rubik-Light.b3de0322.eot"
+  },
+  {
+    "revision": "b4f80748d680aa0bcf80bfb2eb784233",
+    "url": "/appvelox_test/static/media/Rubik-Light.b4f80748.ttf"
+  },
+  {
+    "revision": "cf012899a1ad9dbfcf2fee1dd4f7b0a2",
+    "url": "/appvelox_test/static/media/Rubik-Light.cf012899.woff"
+  },
+  {
+    "revision": "164d06dfa49201410c5bc11de5c05811",
+    "url": "/appvelox_test/static/media/Rubik-Medium.164d06df.woff"
+  },
+  {
+    "revision": "2fb0f7fe08ea2c53741c0fde85736577",
+    "url": "/appvelox_test/static/media/Rubik-Medium.2fb0f7fe.svg"
+  },
+  {
+    "revision": "324c59d7344fd2e5c6a98a9ec246268b",
+    "url": "/appvelox_test/static/media/Rubik-Medium.324c59d7.woff2"
+  },
+  {
+    "revision": "5ba70e5f2b895574a1f852d4b5cd05d9",
+    "url": "/appvelox_test/static/media/Rubik-Medium.5ba70e5f.ttf"
+  },
+  {
+    "revision": "d3a289a29b928d08c065fc0649631bc4",
+    "url": "/appvelox_test/static/media/Rubik-Medium.d3a289a2.eot"
+  },
+  {
+    "revision": "0f878696a3908dbe755b9240c8605b2e",
+    "url": "/appvelox_test/static/media/Rubik-Regular.0f878696.woff2"
+  },
+  {
+    "revision": "21aec918f78c7fc41bf978a31f625b6a",
+    "url": "/appvelox_test/static/media/Rubik-Regular.21aec918.eot"
+  },
+  {
+    "revision": "5d278b91b774c76dae5d47a9f318d737",
+    "url": "/appvelox_test/static/media/Rubik-Regular.5d278b91.woff"
+  },
+  {
+    "revision": "a98027d322d1df9103e35ad7daf598c1",
+    "url": "/appvelox_test/static/media/Rubik-Regular.a98027d3.svg"
+  },
+  {
+    "revision": "b805b7ab7a1ffa920828870fbad47b91",
+    "url": "/appvelox_test/static/media/Rubik-Regular.b805b7ab.ttf"
+  },
+  {
+    "revision": "6092e659609fcc97df3f5f2e651e73c7",
+    "url": "/appvelox_test/static/media/addInfo.6092e659.svg"
+  },
+  {
+    "revision": "7f1d6224045ca80ed9a965971b6c80e5",
+    "url": "/appvelox_test/static/media/analysisResult.7f1d6224.svg"
+  },
+  {
+    "revision": "6679c2a30dee8507d191d37f74dc2f80",
+    "url": "/appvelox_test/static/media/appvelox.6679c2a3.svg"
+  },
+  {
+    "revision": "bafa50c108060218d13dd4f4a59ca023",
+    "url": "/appvelox_test/static/media/arrow_back.bafa50c1.svg"
+  },
+  {
+    "revision": "129d6ec54f6c0e93bc8b58e8c923f9db",
+    "url": "/appvelox_test/static/media/arrow_forward.129d6ec5.svg"
+  },
+  {
+    "revision": "f18a8fa2f41b02c9cbc8d9e1fbb9d338",
+    "url": "/appvelox_test/static/media/avatar.f18a8fa2.svg"
+  },
+  {
+    "revision": "cd7359810422b02785d5a0bcca5d104b",
+    "url": "/appvelox_test/static/media/extand_btn.cd735981.svg"
+  },
+  {
+    "revision": "30dc739d6831e9b6fcb4305197c97899",
+    "url": "/appvelox_test/static/media/help.30dc739d.svg"
+  },
+  {
+    "revision": "4df0ff3023992de91ca28fc7b3037aaa",
+    "url": "/appvelox_test/static/media/left_arrow.4df0ff30.svg"
+  },
+  {
+    "revision": "bcbe03ba07a2eae117d9a4225c733899",
+    "url": "/appvelox_test/static/media/meetingHistory.bcbe03ba.svg"
+  },
+  {
+    "revision": "2091252f76a9afce3d8b402b8af60f63",
+    "url": "/appvelox_test/static/media/notification.2091252f.svg"
+  },
+  {
+    "revision": "0e8f2eb0b64f297c8ee3828f15dc248e",
+    "url": "/appvelox_test/static/media/patientInfo.0e8f2eb0.svg"
+  },
+  {
+    "revision": "a6431a329dd115a243cac6e5e262fc4d",
+    "url": "/appvelox_test/static/media/search.a6431a32.svg"
+  },
+  {
+    "revision": "f141271214ab6495b8a69b7ae1babbe4",
+    "url": "/appvelox_test/static/media/visually_impaired_mode.f1412712.svg"
+  },
+  {
+    "revision": "c2a58d068961c52adbc63443a2e8d078",
+    "url": "/appvelox_test/static/media/white_round_border.c2a58d06.svg"
+  }
+]);
